7c6d353c0355c68d1f75c82cd05a3cd2e4c7a400
Juan <jd.montanez@uniandes.edu.co>
2026-05-11 08:15:11 -0500

    Implement GET and POST for bar events

--- mongo-api.py
+++ mongo-api.py
@@ -39,9 +39,14 @@ def post_comentario(bar_id: int, datos: dict):
     db["comentarios"].insert_one(datos)
     return {'mensaje': 'Comentario guardado'}
 
-# TODO: implementar GET /bares/{bar_id}/eventos
-# Debe retornar todos los eventos del bar desde la colección 'eventos'
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
+    return eventos
 
-# TODO: implementar POST /bares/{bar_id}/eventos  
-# Debe insertar el evento en la colección 'eventos'
-# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha_creacion'] = datetime.now().isoformat()
+    db["eventos"].insert_one(datos)
+    return {'mensaje': 'Evento guardado'}
