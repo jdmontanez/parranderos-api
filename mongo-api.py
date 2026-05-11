36ef7c1a8c3db6838735a727ea857fb767cba52b
Juan <jd.montanez@uniandes.edu.co>
2026-05-11 12:15:48 -0500

    Refactor mongo-api.py for cleaner code structure

--- mongo-api.py
+++ mongo-api.py
@@ -17,55 +17,30 @@ client = MongoClient(os.environ["MONGO_URI"])
 
 db = client["ISIS2304D10202610"]
 
-
 @app.get("/")
 def inicio():
     return {"estado": "API funcionando correctamente"}
 
-
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-
-    comentarios = list(
-        db["comentarios"].find(
-            {"bar_id": bar_id},
-            {"_id": 0}
-        )
-    )
-
+    comentarios = list(db["comentarios"].find({"bar_id": bar_id}, {"_id": 0}))
     return comentarios
-
 
 @app.post('/bares/{bar_id}/comentarios')
 def post_comentario(bar_id: int, datos: dict):
-
     datos['bar_id'] = bar_id
-    datos['fecha'] = datetime.now().isoformat()
-
+    datos['fecha']  = datetime.now().isoformat()
     db["comentarios"].insert_one(datos)
-
     return {'mensaje': 'Comentario guardado'}
-
 
 @app.get('/bares/{bar_id}/eventos')
 def get_eventos(bar_id: int):
-
-    eventos = list(
-        db["eventos"].find(
-            {"bar_id": bar_id},
-            {"_id": 0}
-        )
-    )
-
+    eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
     return eventos
-
 
 @app.post('/bares/{bar_id}/eventos')
 def post_evento(bar_id: int, datos: dict):
-
     datos['bar_id'] = bar_id
     datos['fecha_creacion'] = datetime.now().isoformat()
-
     db["eventos"].insert_one(datos)
-
     return {'mensaje': 'Evento guardado'}
